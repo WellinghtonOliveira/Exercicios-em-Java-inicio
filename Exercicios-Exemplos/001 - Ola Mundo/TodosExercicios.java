026d0d9ec123f0caf76eeb5f3007003707960b56
Wellinghton O. <71161234+WellinghtonOliveira@users.noreply.github.com>
2025-10-28 15:31:23 -0300

    Create TodosExercicios.java

---
+++ Exercicios-Exemplos/001 - Ola Mundo/TodosExercicios.java
@@ -0,0 +1,21 @@
+public class TodosExercicios {
+    public static void main(String[] args) {
+        // calculando a area de um retangulo 
+        int largura, altura, area;
+        
+        largura = 7;
+        altura = 10;
+        area = largura * altura;
+
+        System.out.println("");
+        System.out.println("Printando a área do retangulo:");
+        System.out.println("");
+        System.out.println("==============================");
+        System.out.println("|");
+        System.out.println("| A altura do Retangulo é: " + altura);
+        System.out.println("| A largura do Retangulo é: " + largura);
+        System.out.println("| A area do Retangulo é: " + area);
+        System.out.println("|");
+        System.out.println("===============================");
+    }
+}
