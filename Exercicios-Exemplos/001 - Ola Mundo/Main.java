9c1985227f747449e02bbabcd60a73fa35a6eec1
Wellinghton O. <71161234+WellinghtonOliveira@users.noreply.github.com>
2025-10-28 15:31:26 -0300

    Update Main.java

--- Exercicios-Exemplos/001 - Ola Mundo/Main.java
+++ Exercicios-Exemplos/001 - Ola Mundo/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.print("Não pula linha: ");
         System.out.println("Olá Mundo!");
     }
 }
+
+// Números não são necessario colocar entre aspas
