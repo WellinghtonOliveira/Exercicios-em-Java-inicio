2edaab4f823c8a80cdaf45c93b9b7ce856aa6b79
Wellinghton O. <71161234+WellinghtonOliveira@users.noreply.github.com>
2025-10-28 15:31:39 -0300

    att

---
+++ Exercicios-Exemplos/001 - Ola Mundo/Variaveis.java
@@ -0,0 +1,16 @@
+public class Variaveis {
+    public static void main(String[] args) {
+        /*
+            Tipos de variaveis
+        int myNum = 20;
+        float myfloatNum = 5.10f;
+        char myCharacter = 'W';
+        boolean myBool = true;
+         */
+
+        String name = "Eu mesmo.";
+        
+        System.out.println("Olá " + name);
+
+    }
+}
