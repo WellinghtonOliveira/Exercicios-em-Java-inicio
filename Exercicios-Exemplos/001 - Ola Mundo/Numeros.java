c1bd1baf6a0902387b9c34d7df74dbc5d31c03af
Wellinghton O. <71161234+WellinghtonOliveira@users.noreply.github.com>
2025-10-28 15:31:33 -0300

    Create Numeros.java

---
+++ Exercicios-Exemplos/001 - Ola Mundo/Numeros.java
@@ -0,0 +1,9 @@
+public class Numeros {
+    public static void main(String[] args) {
+        // A linha de baixo e uma soma entre numeros
+        System.out.println(3 + 3);
+        System.out.println(2);
+        System.out.println(10 * 23 - 20 + 1);
+    }
+}
+// Soma entre numeros
