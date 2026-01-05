3d0cd2a1b9a26bd87312c35f6a02bb8b39423ffe
Wellinghton O. <71161234+WellinghtonOliveira@users.noreply.github.com>
2026-01-05 09:38:07 -0300

    Update App.java

--- Exercicios-Exemplos/002/Exercicio - 1/src/App.java
+++ Exercicios-Exemplos/002/Exercicio - 1/src/App.java
@@ -1,29 +1,49 @@
 import java.util.Scanner;
 
 public class App {
+    // static int doubleGame(int x) {
+    //     return x * 2;
+    // }
+    
+    public static int sum(int k) {
+        if (k > 0) {
+            return k + sum(k - 1);
+        }else {
+            return 0;
+        }
+    }
+
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Digite seu primeiro nome aqui: ");
-        String nomeCompleto = scanner.nextLine();
-
-        System.out.println("Olá, " + nomeCompleto + "! Bem-vindo");
         
-        System.out.print("Quantos anos você tem? ");
-        int age = scanner.nextInt();
+        int result = sum(20);
+        System.out.println(result);
 
-        while (age < 18) {
-            System.out.print("Precisa ter no minimo 18 anos: ");
-            age = scanner.nextInt();
-        }
+        // for (int i = 0; i <= 5; i++) {
+        //     System.out.println("O dobro de " + i + " é " + doubleGame(i));
+        // }
 
-        scanner.close();
-        System.out.println("Rodando JAVA");
+
+        // System.out.print("Digite seu primeiro nome aqui: ");
+        // String nomeCompleto = scanner.nextLine();
+
+        // System.out.println("Olá, " + nomeCompleto + "! Bem-vindo");
         
-        myMethod(nomeCompleto);
+        // System.out.print("Quantos anos você tem? ");
+        // int age = scanner.nextInt();
+
+        // while (age < 18) {
+        //     System.out.print("Precisa ter no minimo 18 anos: ");
+        //     age = scanner.nextInt();
+        // }
+
+        // scanner.close();
+        // System.out.println("Rodando JAVA");
+        
+        // myMethod(nomeCompleto);
     }
 
-    static void myMethod(String fname) {
-        System.out.println(fname + " Refsnes");
-    }
+    // static void myMethod(String fname) {
+    //     System.out.println(fname + " Refsnes");
+    // }
 }
