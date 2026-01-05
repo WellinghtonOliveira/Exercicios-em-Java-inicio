95f392df64fb47f491bd9f98bf4536a1f5197dee
Wellinghton O. <71161234+WellinghtonOliveira@users.noreply.github.com>
2026-01-05 09:23:19 -0300

    Update App.java

--- Exercicios-Exemplos/002/Exercicio - 1/src/App.java
+++ Exercicios-Exemplos/002/Exercicio - 1/src/App.java
@@ -2,9 +2,28 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-
         Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Digite seu primeiro nome aqui: ");
+        String nomeCompleto = scanner.nextLine();
+
+        System.out.println("Olá, " + nomeCompleto + "! Bem-vindo");
+        
+        System.out.print("Quantos anos você tem? ");
+        int age = scanner.nextInt();
+
+        while (age < 18) {
+            System.out.print("Precisa ter no minimo 18 anos: ");
+            age = scanner.nextInt();
+        }
+
+        scanner.close();
         System.out.println("Rodando JAVA");
+        
+        myMethod(nomeCompleto);
+    }
+
+    static void myMethod(String fname) {
+        System.out.println(fname + " Refsnes");
     }
 }
