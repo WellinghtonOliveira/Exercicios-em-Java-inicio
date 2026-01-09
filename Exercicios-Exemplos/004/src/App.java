bf1b1992ece9817d22c3325cba0276499c63e9f0
Wellinghton O. <71161234+WellinghtonOliveira@users.noreply.github.com>
2026-01-09 16:55:13 -0300

    Update App.java

--- Exercicios-Exemplos/004/src/App.java
+++ Exercicios-Exemplos/004/src/App.java
@@ -3,22 +3,13 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
+        
+        int[] arrayNums = {};
 
         System.out.println("Vamos analisar alguns números? Você digitará um total de cinco números.");
-        
-        System.out.print("Digite o primeiro número: ");
-        int numPrimeiro = scanner.nextInt();
-
-        System.out.print("Digite o segundo número: ");
-        int numSegundo = scanner.nextInt();
-
-        System.out.print("Digite o terceiro número: ");
-        int numTerceiro = scanner.nextInt();
-
-        System.out.print("Digite o quarto número: ");
-        int numQuarto = scanner.nextInt();
-
-        System.out.print("Digite o quinto número: ");
-        int numQuinto = scanner.nextInt();
+    
+        for (int i = 1; i <= 5; i++) {
+            System
+        }
     }
 }
