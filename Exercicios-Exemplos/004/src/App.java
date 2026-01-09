4be9657fd45b8d12671b2078aea2ef76a5393fc2
Wellinghton O. <71161234+WellinghtonOliveira@users.noreply.github.com>
2026-01-09 17:33:37 -0300

    Update App.java

--- Exercicios-Exemplos/004/src/App.java
+++ Exercicios-Exemplos/004/src/App.java
@@ -4,12 +4,25 @@ public class App {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
         
-        int[] arrayNums = {};
+        int[] arrayNums = new int[5];
 
         System.out.println("Vamos analisar alguns números? Você digitará um total de cinco números.");
     
         for (int i = 1; i <= 5; i++) {
-            System
+            System.out.print("Digite seu " + i + " número: ");
+            arrayNums[i - 1] = scanner.nextInt();    
         }
+
+        for (int n : arrayNums) {
+            int pares = 0;
+            
+            if (n % 2 == 0) {
+                ++pares;
+            }
+
+            System.out.println(n);
+        }
+
+        scanner.close();
     }
 }
