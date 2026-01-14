d2c98d6a9d3868bfcdd67982b34bc35e0430b8a3
Wellinghton O. <71161234+WellinghtonOliveira@users.noreply.github.com>
2026-01-14 14:53:46 -0300

    Update App.java

--- Exercicios-Exemplos/006/src/App.java
+++ Exercicios-Exemplos/006/src/App.java
@@ -1,9 +1,16 @@
 import java.util.Scanner;
 
 public class App {
+    public static int myClass(int a) {
+        return a * 2;
+    }
+
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
 
-        scanner.nextInt();
+        System.out.print("Digite um número para multiplica-lo por 2: ");
+        System.out.print("O resuiltado da multiplicação foi: " + myClass(scanner.nextInt()));
+
+        scanner.close();
     }
 }
