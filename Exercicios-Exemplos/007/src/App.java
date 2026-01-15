300bbaff65e87202c432b7c82ba117aaee9d8226
Wellinghton O. <71161234+WellinghtonOliveira@users.noreply.github.com>
2026-01-15 20:09:24 -0300

    Update App.java

--- Exercicios-Exemplos/007/src/App.java
+++ Exercicios-Exemplos/007/src/App.java
@@ -2,10 +2,17 @@ import java.util.Scanner;
 
 public class App {
     Scanner scanner = new Scanner(System.in);
+    int x;
+
+    public App(int y) {
+        x = y;
+    }
 
     public static void main(String[] args) throws Exception {
 
+        App novoObj = new App(2);
 
+        System.out.println(novoObj.x);
 
     }
 }
