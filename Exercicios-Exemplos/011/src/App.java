83d4476f4ea3c1458b2ec55c103f530b63337c92
Wellinghton O. <71161234+WellinghtonOliveira@users.noreply.github.com>
2026-01-17 10:28:02 -0300

    Update App.java

--- Exercicios-Exemplos/011/src/App.java
+++ Exercicios-Exemplos/011/src/App.java
@@ -1,5 +1,14 @@
+// Um static método pertence á classe, não a um objeto específico. isso significa que você pode chama-lo sem criar um objeto da classe
+
+
 public class App {
+
+    static void myStaticMethod() {
+        System.out.println("Static methods can be callec without creating objects");
+    }
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        myStaticMethod(); // Call the static method
+        App.myStaticMethod(); // Or call it using class name
     }
 }
