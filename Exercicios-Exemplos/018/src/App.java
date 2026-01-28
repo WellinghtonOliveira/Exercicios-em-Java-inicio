f73499566d8e01f81532ca475d4948ad4b2ba9ee
Wellinghton O. <71161234+WellinghtonOliveira@users.noreply.github.com>
2026-01-28 15:42:11 -0300

    Exercicios

---
+++ Exercicios-Exemplos/018/src/App.java
@@ -0,0 +1,85 @@
+import java.util.ArrayList;
+
+class Funcionario {
+    void calcularSalario() {
+        System.out.println("Salario dos Funcionarios");
+    }
+
+    int salario() {
+        System.out.println("Bruto");
+        return 0;
+    }
+}
+
+class FuncionarioCLT extends Funcionario {
+    @Override
+    void calcularSalario() {
+        System.out.println("CLT: 1350/M");
+    }
+
+    @Override
+    int salario() {
+        return 1350;
+    }
+}
+
+class FuncionarioPJ extends Funcionario {
+    @Override
+    void calcularSalario() {
+        System.out.println("PJ: 15/H");
+    }
+
+    @Override
+    int salario() {
+        return 15;
+    }
+}
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        ArrayList<Funcionario> funcionarios = new ArrayList<>();
+        funcionarios.add(new FuncionarioCLT());
+        funcionarios.add(new FuncionarioCLT());
+        funcionarios.add(new FuncionarioCLT());
+        funcionarios.add(new FuncionarioPJ());
+        funcionarios.add(new FuncionarioPJ());
+        funcionarios.add(new FuncionarioPJ());
+
+        int somaSalarialCLT = 0;
+        int somaSalarialPj = 0;
+
+        for (Funcionario f : funcionarios) {
+            if (f instanceof FuncionarioCLT) {
+                somaSalarialCLT += f.salario();
+            }else {
+                somaSalarialPj += f.salario();
+            }
+        }
+
+        System.out.println("Soma dos Salarios dos funcionarios CLT: " + somaSalarialCLT);
+        System.out.println("Soma dos Salarios dos funcionarios PJ:" + somaSalarialPj);
+    }
+}
+
+
+/*
+        Crie um sistema simples de funcionários.
+
+        Requisitos:
+        Classe Funcionario
+        Método calcularSalario()
+        Classe FuncionarioCLT (herda de Funcionario)
+        Salário fixo
+        Classe FuncionarioPJ (herda de Funcionario)
+        Salário baseado em horas trabalhadas
+
+        Objetivo:
+        Criar uma lista de Funcionario
+        Adicionar CLT e PJ
+        Percorrer a lista e chamar calcularSalario()
+
+        📌 Foque em:
+        uso de extends
+        método sobrescrito
+        mesma chamada, comportamentos diferentes
+*/
---
+++ Exercicios-Exemplos/019/src/App.java
@@ -0,0 +1,74 @@
+import java.util.ArrayList;
+
+abstract class Forma {
+    public abstract double calcularArea();
+    void calculo() {
+        System.out.println("calule a area");
+    }
+}
+
+class Quadrado extends Forma {
+    int lado;
+
+    Quadrado(int lado) {
+        this.lado = lado;
+    }
+
+    @Override
+    public double calcularArea() {
+        double area = (lado * lado);
+        System.out.println("Area do quadrado: " + area);
+        return area;
+    }
+}
+
+class Circulo extends Forma {
+    int raio;
+
+    Circulo(int raio) {
+        this.raio = raio;
+    }
+
+    public double calcularArea() {
+        double area = (3.14 * (raio * raio));
+
+        System.out.println("Area do circulo: " + area);
+        return area;
+    }
+}
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        // Forma quadrado = new Quadrado(4);
+        // Forma circulo = new Circulo(4);
+
+        // quadrado.calcularArea();
+        // circulo.calcularArea();
+
+        ArrayList<Forma> formas = new ArrayList<>();
+        formas.add(new Quadrado(5));
+        formas.add(new Quadrado(9));
+        formas.add(new Quadrado(2));
+        formas.add(new Circulo(2));
+        formas.add(new Circulo(4));
+        formas.add(new Circulo(8));
+
+        double areaTotal = 0;
+
+        for (Forma f : formas) {
+            areaTotal += f.calcularArea();
+        }
+
+        System.out.println("A soma de todas as areas são: " + areaTotal);
+    }
+}
+
+
+/*
+
+for (TipoVariavel variavel : colecaoOuArray) {
+    // código a ser executado para cada elemento
+}
+
+*/
