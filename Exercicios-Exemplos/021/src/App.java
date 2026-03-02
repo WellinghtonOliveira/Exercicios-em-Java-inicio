dc368d5d5ba02227a8119f6a64ffa69347ec502b
Wellinghton O. <71161234+WellinghtonOliveira@users.noreply.github.com>
2026-03-02 12:52:19 -0300

    Create App.java

---
+++ Exercicios-Exemplos/021/src/App.java
@@ -0,0 +1,15 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Locale.setDefault(Locale.US);
+        Scanner scanner = new Scanner(System.in);
+
+        double numeroRaio = scanner.nextFloat();
+
+        System.out.printf("A=%.4f", 3.14159 * (numeroRaio * numeroRaio));
+
+        scanner.close();
+    }
+}
