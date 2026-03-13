57e3e2c3fea85121fe609d17b7dca6de571662a6
Wellinghton O. <71161234+WellinghtonOliveira@users.noreply.github.com>
2026-03-13 16:12:07 -0300

    Update App.java

--- Exercicios-Exemplos/025/src/App.java
+++ Exercicios-Exemplos/025/src/App.java
@@ -1,56 +1,83 @@
 import java.util.*;
 
 public class App {
-    public static void main(String[] args) throws Exception {
-        Scanner scanner = new Scanner(System.in);
-    
-        ArrayList<Integer> areaTabua = new ArrayList<>();
 
-        
-        while (scanner.hasNextInt()) {
-            int dimencaoM = scanner.nextInt();
-            int dimencaoN = scanner.nextInt();
-    
-            int larguraTabuas = scanner.nextInt();
-            int tabuasDoadas = scanner.nextInt();
-            
-            int areaDoSalao = calculaAreaSalao(dimencaoM, dimencaoN);
-    
-            int x = scanner.nextInt();
-            areaTabua.add(calculaAreaTabua(larguraTabuas, (x * 100)));
-            
-            System.out.println(calculaMenorUsoTabuas(areaDoSalao, areaTabua, tabuasDoadas));
-            if (dimencaoM == 0 && dimencaoN == 0) break;
-        }
-        
+    static int resolve(int larguraSalao, int comprimentoSalao, int larguraTabua, int[] tabuas) {
 
-    }
+        if (larguraSalao % larguraTabua != 0)
+            return Integer.MAX_VALUE;
 
-    public static int calculaAreaSalao(int a, int b) {
-        return a * b;
-    }
+        int faixas = larguraSalao / larguraTabua;
+        int alvo = comprimentoSalao;
 
-    public static int calculaAreaTabua(int a, int b) {
-        return a * b;
-    }
+        int[] t = Arrays.copyOf(tabuas, tabuas.length);
+        Arrays.sort(t);
 
-    public static int calculaMenorUsoTabuas(int areaSalao, ArrayList<Integer> areaTabua, int quantiaTabua) {
-        ArrayList<Integer> areaT = new ArrayList<>();
+        int i = 0;
+        int j = t.length - 1;
 
-        int areaS = areaSalao;
-        int quantiaT = 0;
+        int usadas = 0;
+        int feitas = 0;
 
-        for (Integer areaCentimetro : areaTabua) {
-            areaT.add(areaCentimetro * 100);
-        }
+        while (i <= j && feitas < faixas) {
 
-        for (Integer i : areaT) {
-            if (areaS > 0) {
-                areaS -= i;
-                quantiaT++;
+            if (t[j] == alvo) {
+                usadas++;
+                feitas++;
+                j--;
+            } else if (t[i] + t[j] == alvo) {
+                usadas += 2;
+                feitas++;
+                i++;
+                j--;
+            } else if (t[i] + t[j] < alvo) {
+                i++;
+            } else {
+                j--;
             }
         }
 
-        return quantiaT;
+        if (feitas == faixas)
+            return usadas;
+        return Integer.MAX_VALUE;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+
+            int m = sc.nextInt();
+            int n = sc.nextInt();
+
+            if (m == 0 && n == 0)
+                break;
+
+            int l = sc.nextInt();
+            int k = sc.nextInt();
+
+            int[] tabuas = new int[k];
+
+            for (int i = 0; i < k; i++) {
+                tabuas[i] = sc.nextInt() * 100;
+            }
+
+            m *= 100;
+            n *= 100;
+            l *= 100;
+
+            int r1 = resolve(m, n, l, tabuas);
+            int r2 = resolve(n, m, l, tabuas);
+
+            int res = Math.min(r1, r2);
+
+            if (res == Integer.MAX_VALUE)
+                System.out.println("impossivel");
+            else
+                System.out.println(res);
+        }
+
+        sc.close();
     }
 }
